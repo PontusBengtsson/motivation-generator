95f1b33b8681a92ac0834570e5732f2c27171a54
PontusBengtsson <pontus.bengtsson@live.com>
2024-05-29 22:06:54 +0200

    Version 2

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-document.getElementById('Group_1061').addEventListener('click', fetchAdvice);
+document.getElementById("Group_1061").addEventListener("click", fetchAdvice);
 
 function fetchAdvice() {
-  fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
       const advice = data.slip;
-      document.getElementById('AdviceNumber').textContent = advice.id;
-      document.getElementById('Text').textContent = `"${advice.advice}"`;
+      document.getElementById("AdviceNumber").textContent = advice.id;
+      document.getElementById("Text").textContent = `"${advice.advice}"`;
     })
-    .catch(error => console.error('Error fetching advice:', error));
+    .catch((error) => console.error("Error fetching advice:", error));
 }
+
+window.onload = function () {
+  function adjustLogoPosition() {
+    const logo = document.getElementById("logo");
+    const container = document.querySelector(".Container");
+    if (window.innerWidth < 600) {
+      logo.style.top = "-70px"; 
+      logo.style.bottom = "unset"; 
+    } else {   
+      logo.style.top = "unset"; 
+      logo.style.bottom = "-90px"; 
+    }
+  }
+
+  
+  adjustLogoPosition();
+
+  
+  window.addEventListener("resize", adjustLogoPosition);
+};
